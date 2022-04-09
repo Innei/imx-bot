12855fce38eb0d9df26691301f7144d7752149b6
Innei <tukon479@gmail.com>
2022-04-09 17:19:02 +0800

    chore: init

---
+++ src/handlers/group/handle-single-message.ts
@@ -0,0 +1,6 @@
+import { GroupMessage, GroupMessageEvent, MessageElem, TextElem } from 'oicq'
+
+export const handleSingleMessage = async (
+  event: GroupMessageEvent,
+  message: MessageElem,
+) => {}
