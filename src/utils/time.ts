7faf3b8ee887d1eb3f6d89263e46e9d93bb700f2
Innei <tukon479@gmail.com>
2022-05-04 18:58:52 +0800

    feat: add relative time for comment notice

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -23,3 +23,32 @@ export const getShortDateTime = (date: Date) => {
     .format(date)
     .replace(/\//g, '-')
 }
+
+export const relativeTimeFromNow = (
+  time: Date | string,
+  current = new Date(),
+) => {
+  time = new Date(time)
+  const msPerMinute = 60 * 1000
+  const msPerHour = msPerMinute * 60
+  const msPerDay = msPerHour * 24
+  const msPerMonth = msPerDay * 30
+  const msPerYear = msPerDay * 365
+
+  const elapsed = +current - +time
+
+  if (elapsed < msPerMinute) {
+    const gap = Math.ceil(elapsed / 1000)
+    return gap <= 0 ? '刚刚' : `${gap} 秒`
+  } else if (elapsed < msPerHour) {
+    return `${Math.round(elapsed / msPerMinute)} 分钟`
+  } else if (elapsed < msPerDay) {
+    return `${Math.round(elapsed / msPerHour)} 小时`
+  } else if (elapsed < msPerMonth) {
+    return `${Math.round(elapsed / msPerDay)} 天`
+  } else if (elapsed < msPerYear) {
+    return `${Math.round(elapsed / msPerMonth)} 个月`
+  } else {
+    return `${Math.round(elapsed / msPerYear)} 年`
+  }
+}
