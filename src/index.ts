0d6054a02553b6d305a204e6ff71e0b191f9f972
Innei <tukon479@gmail.com>
2022-05-22 21:22:33 +0800

    feat: live notify

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { register as registerBilibili } from './modules/bilibili'
 import { register as registerGithubHook } from './modules/github'
 import { register as registerMxSpace } from './modules/mx-space'
 import { registerLogger } from './utils/logger'
@@ -8,5 +9,6 @@ async function bootstrap() {
   client.login()
   client.use(registerMxSpace)
   client.use(registerGithubHook)
+  client.use(registerBilibili)
 }
 bootstrap()
