f3159ddbbfa660ba837bf1f63e96968a5748f815
Innei <tukon479@gmail.com>
2022-10-14 23:09:20 +0800

    feat: draw image2image
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/modules/novelai/api.ts
+++ src/modules/novelai/api.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import { botConfig } from 'config'
+import { URLSearchParams } from 'url'
 
 import { userAgent } from '~/constants/env'
 
 import { disallowedTags } from './ban'
 import { novelAiLogger } from './logger'
 
-const endpoint = 'http://91.217.139.190:5010/got_image'
+const endpoint = 'http://91.217.139.190:5010'
 
 const token = botConfig.novelai.token
 
@@ -29,7 +30,7 @@ export const getApiImage = async (
   const { seed, tagText, scale, shape = 'Portrait' } = params
 
   if (!tagText) {
-    return 'Tag 为空'
+    return ''
   }
   const tagSet = new Set(tagText.split(',').map((t) => t.trim()))
 
@@ -60,10 +61,79 @@ export const getApiImage = async (
   const search = new URLSearchParams(Object.entries(nextParams)).toString()
 
   return await axios
-    .get(`${endpoint}?${search}`, {
+    .get(`${endpoint}/got_image?${search}`, {
       timeout: 60 * 1000,
       headers: {
         'user-agent': userAgent,
+      },
+      responseType: 'arraybuffer',
+    })
+    .then((res) => {
+      novelAiLogger.debug(
+        `get image from novelai: ${res.status}, seed: ${res.headers['seed']}`,
+      )
+      return {
+        buffer: res.data as ArrayBuffer,
+        seed: res.headers['seed'],
+        tags: jointTag,
+      }
+    })
+    .catch((er: any) => {
+      novelAiLogger.debug(er.message)
+      return '生成失败'
+    })
+}
+
+export interface Image2ImageParams {
+  noise: number | string
+  strength: number | string
+
+  image: Buffer
+}
+export const getImage2Image = async (
+  params: Partial<Pick<NovelAIParams, 'tagText' | 'shape'> & Image2ImageParams>,
+) => {
+  const { tagText = '', image } = params
+
+  if (!tagText) {
+    return ''
+  }
+
+  if (!image) {
+    return ''
+  }
+
+  const tagSet = new Set(tagText.split(',').map((t) => t.trim()))
+
+  const jointTag = [...tagSet.values()]
+    .join(',')
+    .replace(
+      new RegExp(`(${disallowedTags.map((tag) => `${tag}|`).join('')})`, 'ig'),
+      '',
+    )
+
+  const nextParams: any = {
+    ...params,
+    tags: jointTag,
+    token,
+  }
+  delete nextParams.tagText
+  delete nextParams.image
+
+  for (const key in nextParams) {
+    if (typeof nextParams[key] === 'undefined') {
+      delete nextParams[key]
+    }
+  }
+
+  const search = new URLSearchParams(Object.entries(nextParams)).toString()
+
+  return axios
+    .post(`${endpoint}/got_image2image?${search}`, image.toString('base64'), {
+      timeout: 60 * 1000,
+      headers: {
+        'user-agent': userAgent,
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
       responseType: 'arraybuffer',
     })
