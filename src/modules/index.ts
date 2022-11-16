fc410057325681344b9c1e6965dcf0c9a39f200a
Innei <tukon479@gmail.com>
2022-11-16 20:50:34 +0800

    fix: omit module name `index`
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -9,7 +9,7 @@ export const registerModules = async () => {
   const modules = await readdir(resolve(__dirname))
 
   modules.forEach((module) => {
-    if (module === 'index.ts') {
+    if (module.startsWith('index.')) {
       return
     }
     logger.log(`register module: ${module}`)
