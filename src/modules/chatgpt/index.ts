4f8d87aa3d533feff5b5ed67c1d53132b683f934
Innei <tukon479@gmail.com>
2023-02-10 11:04:38 +0800

    feat: chatgpt improve
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/modules/chatgpt/index.ts
+++ src/modules/chatgpt/index.ts
@@ -1,4 +1,4 @@
-import cgpt from 'chatgpt-lib'
+import { ChatGPTAPI } from 'chatgpt'
 import { botConfig } from 'config'
 import type { GroupMessageEvent } from 'oicq'
 
@@ -6,10 +6,20 @@ import { commandRegistry } from '~/registries/command'
 import { mentionRegistry } from '~/registries/mention'
 
 export const register = () => {
-  const chatbot = new cgpt.ChatGPT({
-    SessionToken: botConfig.chatgpt.token,
+  const chatbot = new ChatGPTAPI({
+    apiKey: botConfig.chatgpt.token,
   })
+
+  const userId2ConversationIdMap = new Map<number, string>()
+  const conversationId2MessageIdMap = new Map<string, string>()
+
   async function handle(event: GroupMessageEvent) {
+    const userId = event.sender.user_id
+    const conversationId = userId2ConversationIdMap.get(userId)
+    const parentMessageId = conversationId
+      ? conversationId2MessageIdMap.get(conversationId)
+      : undefined
+
     const plainTextMessage = event.message.reduce((acc, cur) => {
       if (cur.type === 'text') {
         acc += cur.text
@@ -17,17 +27,29 @@ export const register = () => {
       return acc
     }, '')
 
-    consola.debug(`ask: ${plainTextMessage}`)
-    const reply = await chatbot.ask(plainTextMessage)
-    event.reply(reply, true)
+    consola.debug(`Q: ${plainTextMessage}`)
+    const reply = await chatbot.sendMessage(plainTextMessage, {
+      conversationId,
+      parentMessageId,
+      timeoutMs: 10_000,
+    })
+
+    if (!conversationId && reply.conversationId) {
+      userId2ConversationIdMap.set(userId, reply.conversationId)
+      conversationId2MessageIdMap.set(reply.conversationId, reply.id)
+    }
+    event.reply(reply.text, true)
   }
   commandRegistry.register('ask', handle)
   commandRegistry.register('chat', (event: GroupMessageEvent) => {
     if (event.message.length === 1 && event.message[0].type === 'text') {
       const isReset = event.message[0].text.trim() === 'reset'
+      const userId = event.sender.user_id
       if (isReset) {
-        chatbot.resetThread()
-        event.reply('已重置对话')
+        const conversationId = userId2ConversationIdMap.get(userId)
+        userId2ConversationIdMap.delete(userId)
+        conversationId && conversationId2MessageIdMap.delete(conversationId)
+        event.reply('ChatGPT: 已重置上下文', true)
         return
       }
     }
